161013be6cebe971d2249113fad506c366fd447a
aldrinjunior <aldrinjunior@gmail.com>
2019-10-19 00:56:35 -0300

    migrations goBarber

--- bootcamp/modulo02/src/app.js
+++ bootcamp/modulo02/src/app.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import routes from './routes';
-//criação da estrutura da aplicaçãoT
-class App{
-    constructor(){
-        this.server = express();
 
-        this.middlewares();
-        this.routes();
-    }
-    middlewares(){
-        this.server.use(express.json());
-    }   
-    routes(){
-        this.server.use(routes)
-    }
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
+
 export default new App().server;
