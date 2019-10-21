d591d20d9c02e30d99ee3741139352ca5a4e3081
aldrinjunior <aldrinjunior@gmail.com>
2019-10-21 15:27:42 -0300

    migrations GoBarber

--- bootcamp/modulo02/src/routes.js
+++ bootcamp/modulo02/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello rocketseat' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'aldrin',
+        email: 'aldrinjunior@rocketseat.com.br',
+        password_hash: '123456789',
+    });
+    return res.json(user);
+});
 
 export default routes;
