fe4dfff572b34d0db1b36801a75b8b2dad41ad08
aldrinjunior <aldrinjunior@gmail.com>
2019-10-28 12:55:27 -0300

    reactIntroConcluido

--- bootcamp/modulo04/src/components/TechList.js
+++ bootcamp/modulo04/src/components/TechList.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import TechItem from './TechItem';
+
 //todo componente escrito em forma de classe precisa do render 
 //criar em formato de classe 
+//as funções para manipular o estado precisam estar no mesmo arquivo 
 class TechList extends Component {
     state = {
         newTech: '',
-        techs: [
-            'Node.js',
-            'React JS',
-            'React Native'
-        ]
+        techs: []
     };
+    //executado assim que o componente aparece em tela
+    componentDidMount() {
+       const techs = localStorage.getItem('techs');
+
+       if (techs) {
+           this.setState({ techs: JSON.parse(techs) });
+       }    
+    }
+    //executado sempre que houver alterações nas props ou estado
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState !== this.state.techs){
+            localStorage.setItem('techs', JSON.stringify(this.state.techs))
+        }
+        //this.props, this.state
+    }
+    //executado quando o compenente deixa de existir
+    componentWillUnmount(){
+//limpar sujeira do componente tipo javascript listener
+    }
     //metodo com cosongole log no valor do input
     //variavel de estado é imutavel, e precisa de setState pra alterar os valores 
     handleInputChange = e => {
@@ -24,11 +42,13 @@ class TechList extends Component {
         this.setState({ techs: [...this.state.techs, this.state.newTech],
         newTech: ''
         });
+
+        
         //nao pode fazer alteraçoes no array e nos objetos, tem que criar ele do zero
     }
 
     handleDelete = (tech) => {
-        
+        this.setState({ techs: this.state.techs.filter(t => t !== tech)}) //só vai retornar as tecnlogias que forem diferentes do tech clicado
     }
 
     render() {
@@ -38,13 +58,12 @@ class TechList extends Component {
             <form onSubmit={this.handleSubmit}>
             <ul> 
                 {this.state.techs.map( tech => (
-                <li key={tech}>
-                  {tech}
-                <button 
-                    onClick={} 
-                    type="button">Remover</button>  
-                </li>
-             ))}   
+                <TechItem 
+                key={tech} 
+                tech={tech} 
+                onDelete={() => this.handleDelete(tech)} 
+                />
+            ))}   
             </ul>
             <input 
                 type="text" 
@@ -62,5 +81,7 @@ class TechList extends Component {
         );
     }
 }
+
+
 
 export default TechList;
