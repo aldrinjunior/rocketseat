a11325b68410597c742494e54adc71a3739f0365
aldrinjunior <aldrinjunior@gmail.com>
2019-10-30 15:43:25 -0300

    quaseConcluido

--- bootcamp/desafio1NodeJS/index.js
+++ bootcamp/desafio1NodeJS/index.js
@@ -7,51 +7,69 @@ server.use(express.json());
 
 const projects= [];//criado um array de projects como uma constante
 
+//contador de requisições
+server.use((req, res, next) => {
+    console.count('a requisição foi chamada');
+    return next();
+});
+
+function checkProjectExists(req, res, next) { //server para checar se o id existe dentro do array de projetos
+    if (!req.body.id) {
+    return res.status(400).json({ error: 'Project not found !'});
+    }//se existir, continua com a função next
+    return next();
+};
+
 //buscar todos projetos
 server.get('/projects', (req, res) => { 
     return res.json(projects);
 });
 
 //buscar um projeto pelo id
-server.get('/projects/:id', (req, res) => { //os routes params precisa estar dentro da rota também 
+server.get('/projects/:id', checkProjectExists, (req, res) => { //os routes params precisa estar dentro da rota também 
     const { id } =req.params; 
     return res.json(projects[id]);
 });
 
 //criação de um projeto (falta arrumar as tasks)
 server.post('/projects', (req, res) => {
-    const { id, title } = req.body;
+    const { id, title, tasks } = req.body;
 
     const project = {
         id,
         title,
-        tasks: []
+        tasks 
     };
-
     projects.push(project); //push serve para ?
 
     return res.json(projects);
 });
 //alterar as rotas 
-server.put('/projects/:id', (req, res) => {
-    const { id, title } = req.body;
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+    const { id, title, tasks } = req.body;
 
     const project = {
         id, 
         title,
-        tasks: []
+        tasks
     };
-
     projects [id] = project;
     return res.json(projects)
 });
-//apaga por id 
+//apaga por id (check nao funciona)
 server.delete('/projects/:id', (req, res) => {
-    const {id} = req.params;
-
     projects.splice(id, 1);
+    return res.send(projects);
+});
 
+//criação de uma task dentro do projeto existente (nao funciona adicionar tarefa )
+server.post('/projects/:id', checkProjectExists, (req, res) => {
+    const { id, title, tasks } = req.body;
+
+    const project = {
+        tasks
+    };
+    projects [tasks] = project.tasks;
     return res.json(projects);
-})
-
+});
 server.listen(4000);
