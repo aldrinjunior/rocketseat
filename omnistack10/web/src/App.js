2b4aeeff9b53bba00378e892ed017f15c320b034
aldrinjunior <aldrinjunior@gmail.com>
2020-01-20 00:07:01 -0300

    parte do cadastro

--- omnistack10/web/src/App.js
+++ omnistack10/web/src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
-
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 //Componente = é uma função isolada de (html, css, js) que retorna algum conteúdo sem interferir no restante da aplicação
 //Propriedade = informações que um componente pai passa para um componente filho
-//Estado = 
-
+//Estado = informação mantida pelo componente (imutabilidade, nunca altera, vamos criar um novo a partir do anterior)
 function App() {
-  const counter= 0;
-  function incrementCounter() {
-    alert('Hello');
-  }
-
   return (
-  <>  
-    <h1>Contador: {counter}</h1>
-    <button onClick={incrementCounter}>Incrementar</button>
-  </>
+    <div id="app">
+        <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div class="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id="latitude" required/>
+          </div>
+          <div class="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input name="longitude" id="longitude" required/>
+          </div>
+        </div>
+        <button type="submit">Salvar</button>
+           </form>
+          </aside>
+        <main>
+
+        </main>
+    </div>
   );
 }
 
