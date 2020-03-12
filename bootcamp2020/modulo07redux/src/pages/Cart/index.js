350e1d2cc736e832f248bcd06d0ab7d1a1ff2daa
aldrinjunior <aldrinjunior@gmail.com>
2020-03-12 00:33:22 -0300

    bootcamp2020_styles

--- bootcamp2020/modulo07redux/src/pages/Cart/index.js
+++ bootcamp2020/modulo07redux/src/pages/Cart/index.js
@@ -3,5 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <div />
+  );
 }
--- bootcamp2020/modulo07redux/src/pages/Home/index.js
+++ bootcamp2020/modulo07redux/src/pages/Home/index.js
@@ -1,9 +1,102 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {MdAddShoppingCart} from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+        <li>
+          <img 
+            src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+            alt="Tenis"
+          />
+        <strong>Tenis muito legal</strong>
+        <span>$ 129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16} color = "#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li>    
+    <li>
+          <img 
+            src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+            alt="Tenis"
+          />
+        <strong>Tenis muito legal</strong>
+        <span>$ 129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16} color = "#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li> 
+    <li>
+          <img 
+            src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+            alt="Tenis"
+          />
+        <strong>Tenis muito legal</strong>
+        <span>$ 129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16} color = "#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li> 
+    <li>
+          <img 
+            src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+            alt="Tenis"
+          />
+        <strong>Tenis muito legal</strong>
+        <span>$ 129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16} color = "#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li> 
+    <li>
+          <img 
+            src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+            alt="Tenis"
+          />
+        <strong>Tenis muito legal</strong>
+        <span>$ 129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16} color = "#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li> 
+    <li>
+          <img 
+            src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-06/2592_Trio3.jpg"
+            alt="Tenis"
+          />
+        <strong>Tenis muito legal</strong>
+        <span>$ 129,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size = {16} color = "#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+    </button>
+    </li> 
+    </ProductList>
   );
 }
