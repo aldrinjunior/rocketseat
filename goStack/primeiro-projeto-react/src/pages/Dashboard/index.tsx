a890c3b07f6baeb088f320ad13edc9ea3e51727f
aldrinjunior <aldrinjunior@gmail.com>
2020-04-21 14:51:07 -0300

    estizando repositories

--- goStack/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ goStack/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,10 +1,59 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 //function Dashboard() {}
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          <img
+            src="https://avatars2.githubusercontent.com/u/38501933?s=460&u=c37d8f46992a314dd8ca5ee6c6f7f435bdca7f2d&v=4"
+            alt="Aldrin Junior"
+          />
+          <div>
+            <strong>rocketseat unform</strong>
+            <p>Easy peasy scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="">
+          <img
+            src="https://avatars2.githubusercontent.com/u/38501933?s=460&u=c37d8f46992a314dd8ca5ee6c6f7f435bdca7f2d&v=4"
+            alt="Aldrin Junior"
+          />
+          <div>
+            <strong>rocketseat unform</strong>
+            <p>Easy peasy scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="">
+          <img
+            src="https://avatars2.githubusercontent.com/u/38501933?s=460&u=c37d8f46992a314dd8ca5ee6c6f7f435bdca7f2d&v=4"
+            alt="Aldrin Junior"
+          />
+          <div>
+            <strong>rocketseat unform</strong>
+            <p>Easy peasy scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- goStack/primeiro-projeto-react/src/styles/global.ts
+++ goStack/primeiro-projeto-react/src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import github_background from '../assets/github_background.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -9,7 +11,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F0F0F5;
+    background: #F0F0F5 url(${github_background}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
   }
   body, input, button {
@@ -19,5 +21,8 @@ export default createGlobalStyle`
     max-width: 960px;
     margin: 0 auto;
     padding: 40px 20px;
+  }
+  button {
+    cursor: pointer;
   }
 `;
