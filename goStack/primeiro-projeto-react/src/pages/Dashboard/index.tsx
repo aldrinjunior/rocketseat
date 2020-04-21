d829f1e7989a9e42ce07b661952d3b8609fceec6
aldrinjunior <aldrinjunior@gmail.com>
2020-04-21 11:49:35 -0300

    primeiroProjetoReact

---
+++ goStack/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Title } from './styles';
+//function Dashboard() {}
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositórios no Github</Title>;
+};
+
+export default Dashboard;
