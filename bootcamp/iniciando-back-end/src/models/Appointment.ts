202188ff377ce07032aeb86190a9b62da256a054
aldrinjunior <aldrinjunior@gmail.com>
2020-05-11 16:28:13 -0300

    backup backend

---
+++ bootcamp/iniciando-back-end/src/models/Appointment.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import User from './User';
+// Quando utiliza o decorator, significa que ele pega todo o
+// codigo e indica que essa classe precisa ser armazenada na tab de appointments
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Appointment;
+
+// como falar para o Typescript que o contructor vai receber todas as variaveis, menos o id
---
+++ bootcamp/iniciando-back-end/src/services/CreateAppointmentService.ts
@@ -0,0 +1,44 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+// [x] recebimento das informações
+// [x] resposta precisa de uma tratativa de erros e excess˜oes
+// [x] acesso ao respositório
+
+// request quando está recebendo os dados
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentsInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentsInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+    // o create apenas cria, mas nao salva no banco
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
