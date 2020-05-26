3e036b10e9b989a80115ad48ea8ee9ee13fce189
aldrinjunior <aldrinjunior@gmail.com>
2020-05-26 16:29:14 -0300

    Template Engine Finalizado

---
+++ bootcamp/05-continuando-back-end/dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: '116f764d281b512357ae3ce86b39b9b8',
+        expiresIn: '1d',
+    },
+};
---
+++ bootcamp/05-continuando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,47 @@
+import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+// [x] recebimento das informações
+// [x] resposta precisa de uma tratativa de erros e excess˜oes
+// [x] acesso ao respositório
+
+// request quando está recebendo os dados
+
+interface IRequest {
+  provider_id: string;
+  date: Date;
+}
+
+@injectable()
+class CreateAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentsInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentsInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = await this.appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
