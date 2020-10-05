feb6780aa819161f40dcd33bce11884f23a077fb
Aldrin <aldrinjunior@gmail.com>
2020-10-04 21:51:55 -0300

    listando-projetos-da-api

--- bootcamp_gostack/conceitos-dev/frontend/src/App.js
+++ bootcamp_gostack/conceitos-dev/frontend/src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-import Header from './Components/Header';
+import './App.css';
+import Header from './components/Header';
 
 function App() {
   const [projects, setProjects] = useState([
     'Desenvolvimento de App',
     'Frontend Web',
   ]);
+
+  // usar async await ou o then, porque vai demorar um pouco
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      console.log(response);
+    });
+  }, []); // se colocar uma variavel dentro de [],
+  // ela vai atualizar sempre que ela for alterada
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -15,6 +25,7 @@ function App() {
   return (
     <>
       <Header title='Projects' />
+
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
